12703c7b5ed9684f45e7088afee4d262c29dac85
Daniel Mateos <daniel@mateos.cc>
2016-05-29 05:43:22 +1000

    fixed test broken due to subject visibility thing

--- spec/requests/subjects_a_z_page_spec.rb
+++ spec/requests/subjects_a_z_page_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe "Subject A-Z listing page /subjects" do
   context "listing subjects" do 
     let!(:subject_one) { FactoryGirl.create(:subject, name: "A test subject") }
     let!(:subject_two) { FactoryGirl.create(:subject, name: "B test subject") }
+    let!(:organisation) { FactoryGirl.create(:organisation, name: "test", subjects: [ subject_one, subject_two ]) }
 
-    it "shows all the subjects" do
+    it "shows all the subjects with orgs in them" do
       visit subjects_path
       expect(page).to have_content("A test subject")
       expect(page).to have_content("B test subject")
