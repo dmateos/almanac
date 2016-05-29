bf4dbeb2062e03c0e701918bec5e6a0b1e205527
Daniel Mateos <daniel@mateos.cc>
2016-05-30 02:16:07 +1000

    start on search spec

---
+++ spec/requests/search_organisation_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe "Search /search/" do
+  it "loads the search response page when a query is made from /" do
+    visit root_path
+    fill_in("search", with: "test org")
+    click_button("submit")
+    expect(page).to have_content("Search result")
+  end
+
+  context "searching for organisations" do
+    it "searches for a given organisation and the organisation is found" do
+      visit root_path
+      fill_in("search", with: "test org")
+      click_button("submit")
+      expect(page).to have_content("test org")
+    end
+
+    it "does not include an irrelevent organisation in results" do
+      visit root_path
+      fill_in("search", with: "whatever man")
+      click_button("submit")
+      expect(page).to_not have_content("test org")
+    end
+  end
+
+  context "searching for organisations limited by council" do
+    it "searches for a given organisation and a result is found within that council" do
+
+    end
+
+    it "does not return a matching result that is not in that council" do
+
+    end
+  end
+end
